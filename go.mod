44e515dfdf4e096957aea53a53923a2a279667c5
fadjar.iftikhar <fadjar.iftikhar@gmail.com>
2024-10-20 01:57:11 +0000

    login create token

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/iftikharfadjar/playground_go.git
 
 go 1.21.11
+
+require github.com/go-chi/chi/v5 v5.1.0 // indirect
---
+++ main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/login", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello"))
+	})
+
+	http.ListenAndServe(":3000", r)
+}
